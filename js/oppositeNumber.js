19561fa6c794e9e7b4fc6a2e1c637acc66cc0508
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-12-02 14:25:48 -0500

    opposite number

---
+++ js/oppositeNumber.js
@@ -0,0 +1,26 @@
+https://www.codewars.com/kata/opposite-number
+
+function opposite(number) {
+    const opposite = -number
+    return opposite
+  }
+
+Test Results:
+
+Test Passed: Value == -1
+Test Passed: Value == 0
+Test Passed: Value == -4.25
+Test Passed: Value == -3.3333333
+Test Passed: Value == 12525220.3325
+Test Passed: Value == 5
+You have passed all of the tests! :)
+
+Some other answers:
+
+function opposite(number) {
+    return(-number);
+  }
+
+  function opposite(number) {
+    return number * (-1);
+}
